ed436a5b0756cfe9258360c5553bf67782d2012d
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-06 19:47:25 +0200

    Dymanic Tailwind Colors Added

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
-
+  toggleGocare() {
+    const body = document.body;
+    body.classList.toggle('gocare'); // Toggle the 'gocare' class
+  }
 }
