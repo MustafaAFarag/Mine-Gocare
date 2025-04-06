3cc4d0d0321a2195cfcb16fd952146d7d054f5b1
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-06 17:07:52 +0200

    First Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './pages/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/homepage/homepage.component').then(
+            (m) => m.HomepageComponent
+          ),
+      },
+    ],
+  },
+];
---
+++ src/app/pages/product-collection/product-collection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-collection',
+  imports: [],
+  templateUrl: './product-collection.component.html',
+  styleUrl: './product-collection.component.css'
+})
+export class ProductCollectionComponent {
+
+}
