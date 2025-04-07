48c71ab1e4f158f6bd97fdcaa4e088452295b6a1
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-07 02:55:20 +0200

    Images Added and Typography Font and Sizes and Colors Adjusted

--- src/app/features/homepage/instashop-section/instashop-section.component.ts
+++ src/app/features/homepage/instashop-section/instashop-section.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 })
 export class InstashopSectionComponent {
   instaPosts = [
-    { id: 1, image: 'https://placehold.co/600x600/E34F30/white?text=Insta+1' },
-    { id: 2, image: 'https://placehold.co/600x600/7D56F1/white?text=Insta+2' },
-    { id: 3, image: 'https://placehold.co/600x600/F7D969/white?text=Insta+3' },
-    { id: 4, image: 'https://placehold.co/600x600/47B16E/white?text=Insta+4' },
-    { id: 5, image: 'https://placehold.co/600x600/3DC1F2/white?text=Insta+5' },
+    { id: 1, image: 'assets/images/settings/insta6-berry.jpg' },
+    { id: 2, image: 'assets/images/settings/insta7-berry.jpg' },
+    { id: 3, image: 'assets/images/settings/insta2-berry.jpg' },
+    { id: 4, image: 'assets/images/settings/insta1-berry.jpg' },
+    { id: 5, image: 'assets/images/settings/insta4-berry.jpg' },
   ];
 }
