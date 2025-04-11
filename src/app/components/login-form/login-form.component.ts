ddbb5d4613ecfde374787f90a97bc452c8e7dcb7
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-11 22:49:32 +0200

    Login Click Console loged and Responivesness

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,16 +1,29 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.css'],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   providers: [],
 })
 export class LoginFormComponent {
   @Output() toggle = new EventEmitter<boolean>();
+
+  loginObject = {
+    email: '',
+    password: '',
+  };
+
+  constructor(private router: Router) {}
+
+  onLoginSubmit() {
+    console.log('Login form submitted:', this.loginObject);
+  }
 
   // Emit the toggle event to switch to the signup form
   toggleMode() {
