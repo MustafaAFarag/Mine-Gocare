0717e82728157856a5bd1240cc9e764b014b9ae6
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-13 11:25:18 +0200

    Cart UI done

--- src/app/features/product-details/breadcrumb/breadcrumb.component.ts
+++ src/app/features/product-details/breadcrumb/breadcrumb.component.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.css',
 })
-export class BreadcrumbComponent {}
+export class BreadcrumbComponent implements OnInit {
+  @Input() name?: string;
+  @Input() soloRoute?: string;
+  currentRoute: string = '';
+
+  constructor(
+    public router: Router,
+    private route: ActivatedRoute,
+  ) {
+    this.router.events.subscribe(() => {
+      this.currentRoute = this.router.url;
+    });
+  }
+
+  ngOnInit(): void {
+    console.log('CURRENT ROUTE', this.router.url);
+  }
+}
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartItem, CART_STORAGE_KEY } from '../../model/Cart';
 import { getFullImageUrl } from '../../lib/utils';
+import { BreadcrumbComponent } from '../../features/product-details/breadcrumb/breadcrumb.component';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BreadcrumbComponent],
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
 })
