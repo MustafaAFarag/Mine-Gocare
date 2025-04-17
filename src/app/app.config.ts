7eeee710e45099684e24f15a350e92e88bdbc700
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-17 15:42:54 +0200

    Added Localization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,8 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,10 +10,20 @@ import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpClient,
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +31,18 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
+    importProvidersFrom([
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient],
+        },
+        defaultLanguage: 'en',
+      }),
+    ]),
+    provideHttpClient(withInterceptorsFromDi()),
+
     provideAnimationsAsync(),
     providePrimeNG({
       theme: {
--- src/app/components/navbar-banner/navbar-banner.component.ts
+++ src/app/components/navbar-banner/navbar-banner.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar-banner',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './navbar-banner.component.html',
   styleUrl: './navbar-banner.component.css',
 })
-export class NavbarBannerComponent {}
+export class NavbarBannerComponent {
+  translate: TranslateService = inject(TranslateService);
+
+  translateLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+}
