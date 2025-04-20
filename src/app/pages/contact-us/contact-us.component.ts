e2b3eb5d8ee1289242698e5d4f87ab057916f29b
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-20 14:48:36 +0200

    Contact us Done

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BreadcrumbComponent } from '../../shared/breadcrumb/breadcrumb.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, BreadcrumbComponent],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    BreadcrumbComponent,
+    TranslateModule,
+  ],
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css'],
 })
