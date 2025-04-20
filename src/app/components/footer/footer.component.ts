fcc6b8a34408eb2d510e445f1588eb75e78480ff
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-20 15:25:51 +0200

    Footer Localized

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductService } from '../../services/product.service';
 import { Category } from '../../model/Categories';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
 export class FooterComponent implements OnInit {
   categories: Category[] = [];
+  private languageService = inject(LanguageService);
 
   constructor(
     private router: Router,
@@ -21,10 +24,17 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
     this.getCategoriesAPI();
   }
+
   getCategoriesAPI() {
     this.productService.getCategories().subscribe((res) => {
       this.categories = res.result;
     });
+  }
+
+  getCategoryName(category: any): string {
+    // Get the current language from the service
+    const currentLang = this.languageService.getCurrentLanguage();
+    return currentLang === 'ar' ? category.name.ar : category.name.en;
   }
 
   navigateToCategory(category: Category): void {
