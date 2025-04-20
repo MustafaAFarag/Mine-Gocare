67849cbd744a1bf465e2472d529c2aec1f3969c5
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-20 14:26:58 +0200

    Instashop and Homepage Done

--- src/app/features/homepage/instashop-section/instashop-section.component.ts
+++ src/app/features/homepage/instashop-section/instashop-section.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-instashop-section',
-  imports: [CommonModule],
+  imports: [CommonModule, TranslateModule],
   templateUrl: './instashop-section.component.html',
   styleUrl: './instashop-section.component.css',
 })
