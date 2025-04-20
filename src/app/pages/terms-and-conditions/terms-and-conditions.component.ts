11157d9aa1aa56fb770f5c3d8858e9a26d6e3629
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-04-20 15:20:08 +0200

    Terms and Conditions Done

--- src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
+++ src/app/pages/terms-and-conditions/terms-and-conditions.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-terms-and-conditions',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './terms-and-conditions.component.html',
-  styleUrl: './terms-and-conditions.component.css'
+  styleUrl: './terms-and-conditions.component.css',
 })
-export class TermsAndConditionsComponent {
-
-}
+export class TermsAndConditionsComponent {}
