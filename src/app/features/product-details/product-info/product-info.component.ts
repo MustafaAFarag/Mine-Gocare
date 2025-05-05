57606d7dcbbe269f0de0a848519a54af174b6767
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-05 12:24:49 +0300

    Product Details Add to cart Bug Fixed

--- src/app/features/product-details/product-info/product-info.component.ts
+++ src/app/features/product-details/product-info/product-info.component.ts
@@ -6,6 +6,7 @@ import { CartItem } from '../../../model/Cart';
 import { CartSidebarService } from '../../../services/cart-sidebar.service';
 import { LanguageService } from '../../../services/language.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { WishlistService } from '../../../services/wishlist.service';
 
 @Component({
   selector: 'app-product-info',
@@ -16,6 +17,7 @@ import { TranslateModule } from '@ngx-translate/core';
 })
 export class ProductInfoComponent {
   private _productDetails!: ProductDetails;
+  isInWishlist: boolean = false;
 
   @Input()
   set productDetails(value: ProductDetails) {
@@ -23,6 +25,7 @@ export class ProductInfoComponent {
     if (value && value.productVariants?.length > 0) {
       this.unitPrice = value.productVariants[0]?.priceAfterDiscount ?? 0;
       this.totalPrice = this.unitPrice;
+      this.checkWishlistStatus();
     }
   }
   get productDetails() {
@@ -49,23 +52,66 @@ export class ProductInfoComponent {
     private cartService: CartService,
     private cartSidebarService: CartSidebarService,
     public languageService: LanguageService,
+    private wishlistService: WishlistService,
   ) {}
 
+  private checkWishlistStatus(): void {
+    if (this.productDetails) {
+      this.isInWishlist = this.wishlistService.isInWishlist(
+        this.productDetails.id,
+        this.productDetails.productVariants[0].id,
+      );
+    }
+  }
+
+  toggleWishlist(): void {
+    if (this.productDetails) {
+      if (this.isInWishlist) {
+        this.wishlistService.removeFromWishlist(
+          this.productDetails.id,
+          this.productDetails.productVariants[0].id,
+        );
+      } else {
+        this.wishlistService.addToWishlist({
+          productId: this.productDetails.id,
+          variantId: this.productDetails.productVariants[0].id,
+          mainImageUrl: this.productDetails.mainImageUrl,
+          name: this.productDetails.productName,
+          brand: this.productDetails.brandName,
+          priceBeforeDiscount:
+            this.productDetails.productVariants[0].priceBeforeDiscount,
+          priceAfterDiscount:
+            this.productDetails.productVariants[0].priceAfterDiscount,
+          discountAmount: this.productDetails.productVariants[0].discountAmount,
+          discountPercentage:
+            this.productDetails.productVariants[0].discountPercentage,
+          currency: this.productDetails.productVariants[0].currency,
+          stockCount: this.productDetails.productVariants[0].stockCount,
+          variantProductNumber: 0,
+          lowQuantity: this.productDetails.productVariants[0].hasLowQuantity,
+          variantQuantityInCart: 0,
+          variantInCartId: 0,
+          isInWishlist: true,
+          promoCodeDetail: null,
+          productVariants: [],
+          rating: this.productDetails.productVariants[0].rating,
+        });
+      }
+      this.checkWishlistStatus();
+    }
+  }
+
   addToCart(product: ProductDetails): void {
-    // Log the incoming product details for debugging
-
-    const currentLang = this.languageService.getCurrentLanguage();
-
     // Create the CartItem object
     const item: CartItem = {
       productId: product.id,
       variantId: product.productVariants[0].id,
-      name:
-        currentLang === 'ar' ? product.productName.ar : product.productName.en,
+      name: product.productName,
       afterPrice: product.productVariants[0].priceAfterDiscount,
       beforePrice: product.productVariants[0].priceBeforeDiscount,
       quantity: this.counter,
       image: product.mainImageUrl,
+      currency: product.productVariants[0].currency,
     };
 
     // Add the item to the cart using the CartService
