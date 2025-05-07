d64376d091b2b16db977092212e88bb7ae5ea6c1
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-07 17:54:29 +0300

    Production Environments Added

---
+++ src/enviroments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  baseURL: 'https://berriesweb.salonspace1.com', // This represents the base URL for running our frontend project.
+  URL: 'https://berriesweb.salonspace1.com/assets/data', // Change only the domain part, keeping "/api" intact
+  apiUrl: 'https://gocare-back-develop.salonspace1.com',
+  storageURL: 'https://berriesweb.salonspace1.com/assets', // Change only the laravel primary domain
+};
