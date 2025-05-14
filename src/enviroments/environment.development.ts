f6fd3bdb1c3416dc84ca57125ec43e9a22f9dd5f
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-14 22:37:49 +0300

    GoCare Pictures and Enviroments Variables Adjusted

--- src/enviroments/environment.development.ts
+++ src/enviroments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: true,
+  production: false,
   baseURL: 'https://berriesweb.salonspace1.com', // This represents the base URL for running our frontend project.
   URL: 'https://berriesweb.salonspace1.com/assets/data', // Change only the domain part, keeping "/api" intact
-  apiUrl: 'https://system.go-care.com',
+  apiUrl: 'https://gocare-back-develop.salonspace1.com',
   storageURL: 'https://berriesweb.salonspace1.com/assets', // Change only the laravel primary domain
 };
