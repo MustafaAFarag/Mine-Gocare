a2099cb7bafb493bebe60f185ad0e5b18db06845
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-05-15 14:37:46 +0300

    Navbar Responsive and Egypt and Saudi Adjusted

--- src/app/components/navbar-banner/navbar-banner.component.ts
+++ src/app/components/navbar-banner/navbar-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, HostListener } from '@angular/core';
+import { Component, inject, HostListener, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service';
 import { NgIf, NgClass } from '@angular/common';
@@ -10,9 +10,11 @@ import { NgIf, NgClass } from '@angular/common';
   templateUrl: './navbar-banner.component.html',
   styleUrl: './navbar-banner.component.css',
 })
-export class NavbarBannerComponent {
+export class NavbarBannerComponent implements OnInit {
   private languageService = inject(LanguageService);
   isLanguageDropdownOpen = false;
+  isCountryDropdownOpen = false;
+  currentCountry = 'EG';
 
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
@@ -20,15 +22,37 @@ export class NavbarBannerComponent {
     if (!target.closest('.language-dropdown')) {
       this.isLanguageDropdownOpen = false;
     }
+    if (!target.closest('.country-dropdown')) {
+      this.isCountryDropdownOpen = false;
+    }
+  }
+
+  ngOnInit() {
+    const savedCountry = localStorage.getItem('country');
+    if (savedCountry) {
+      this.currentCountry = savedCountry;
+    } else {
+      localStorage.setItem('country', 'EG');
+    }
   }
 
   toggleLanguageDropdown() {
     this.isLanguageDropdownOpen = !this.isLanguageDropdownOpen;
   }
 
+  toggleCountryDropdown() {
+    this.isCountryDropdownOpen = !this.isCountryDropdownOpen;
+  }
+
   translateLanguage(lang: string) {
     this.languageService.setLanguage(lang);
     this.isLanguageDropdownOpen = false;
+  }
+
+  selectCountry(country: string) {
+    this.currentCountry = country;
+    localStorage.setItem('country', country);
+    this.isCountryDropdownOpen = false;
   }
 
   getCurrentLanguage(): string {
