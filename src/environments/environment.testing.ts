f06c9e943c86f47e7c6111061bb39e73ff89b68e
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-06-03 15:21:21 +0300

    Website ID for Testing Done with Envrioment

---
+++ src/environments/environment.testing.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseURL: 'https://berriesweb.salonspace1.com', // This represents the base URL for running our frontend project.
+  URL: 'https://berriesweb.salonspace1.com/assets/data', // Change only the domain part, keeping "/api" intact
+  apiUrl: 'https://gocare-back-testing.salonspace1.com/',
+  storageURL: 'https://berriesweb.salonspace1.com/assets', // Change only the laravel primary domain
+};
