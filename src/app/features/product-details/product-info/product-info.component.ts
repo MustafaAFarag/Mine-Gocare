1ff352bf0f794f73de07bfe07aaa36e0eba90073
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-06-16 13:54:23 +0300

    Product Variants Added

--- src/app/features/product-details/product-info/product-info.component.ts
+++ src/app/features/product-details/product-info/product-info.component.ts
@@ -7,23 +7,31 @@ import { CartSidebarService } from '../../../services/cart-sidebar.service';
 import { LanguageService } from '../../../services/language.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { WishlistService } from '../../../services/wishlist.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-info',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, FormsModule],
   templateUrl: './product-info.component.html',
   styleUrl: './product-info.component.css',
 })
 export class ProductInfoComponent {
   private _productDetails!: ProductDetails;
   isInWishlist: boolean = false;
+  selectedVariant: any;
+  selectedColor: string = '';
+  selectedSize: string = '';
 
   @Input()
   set productDetails(value: ProductDetails) {
     this._productDetails = value;
     if (value && value.productVariants?.length > 0) {
-      this.unitPrice = value.productVariants[0]?.priceAfterDiscount ?? 0;
+      // Set initial selected variant
+      this.selectedVariant =
+        value.productVariants.find((v) => v.isSelected) ||
+        value.productVariants[0];
+      this.unitPrice = this.selectedVariant?.priceAfterDiscount ?? 0;
       this.totalPrice = this.unitPrice;
       this.checkWishlistStatus();
     }
@@ -59,7 +67,7 @@ export class ProductInfoComponent {
     if (this.productDetails) {
       this.isInWishlist = this.wishlistService.isInWishlist(
         this.productDetails.id,
-        this.productDetails.productVariants[0].id,
+        this.selectedVariant.id,
       );
     }
   }
@@ -69,55 +77,57 @@ export class ProductInfoComponent {
       if (this.isInWishlist) {
         this.wishlistService.removeFromWishlist(
           this.productDetails.id,
-          this.productDetails.productVariants[0].id,
+          this.selectedVariant.id,
         );
       } else {
         this.wishlistService.addToWishlist({
           productId: this.productDetails.id,
-          variantId: this.productDetails.productVariants[0].id,
-          mainImageUrl: this.productDetails.mainImageUrl,
+          variantId: this.selectedVariant.id,
+          mainImageUrl:
+            this.selectedVariant.mainImageUrl ||
+            this.productDetails.mainImageUrl,
           name: this.productDetails.productName,
           brand: this.productDetails.brandName,
-          priceBeforeDiscount:
-            this.productDetails.productVariants[0].priceBeforeDiscount,
-          priceAfterDiscount:
-            this.productDetails.productVariants[0].priceAfterDiscount,
-          discountAmount: this.productDetails.productVariants[0].discountAmount,
-          discountPercentage:
-            this.productDetails.productVariants[0].discountPercentage,
-          currency: this.productDetails.productVariants[0].currency,
-          stockCount: this.productDetails.productVariants[0].stockCount,
+          priceBeforeDiscount: this.selectedVariant.priceBeforeDiscount,
+          priceAfterDiscount: this.selectedVariant.priceAfterDiscount,
+          discountAmount: this.selectedVariant.discountAmount,
+          discountPercentage: this.selectedVariant.discountPercentage,
+          currency: this.selectedVariant.currency,
+          stockCount: this.selectedVariant.stockCount,
           variantProductNumber: 0,
-          lowQuantity: this.productDetails.productVariants[0].hasLowQuantity,
+          lowQuantity: this.selectedVariant.hasLowQuantity,
           variantQuantityInCart: 0,
           variantInCartId: 0,
           isInWishlist: true,
           promoCodeDetail: null,
           productVariants: [],
-          rating: this.productDetails.productVariants[0].rating,
+          rating: this.selectedVariant.rating,
         });
       }
       this.checkWishlistStatus();
     }
   }
 
+  onVariantSelect(variant: any): void {
+    this.selectedVariant = variant;
+    this.unitPrice = variant.priceAfterDiscount;
+    this.totalPrice = this.counter * this.unitPrice;
+    this.checkWishlistStatus();
+  }
+
   addToCart(product: ProductDetails): void {
-    // Create the CartItem object
     const item: CartItem = {
       productId: product.id,
-      variantId: product.productVariants[0].id,
+      variantId: this.selectedVariant.id,
       name: product.productName,
-      afterPrice: product.productVariants[0].priceAfterDiscount,
-      beforePrice: product.productVariants[0].priceBeforeDiscount,
+      afterPrice: this.selectedVariant.priceAfterDiscount,
+      beforePrice: this.selectedVariant.priceBeforeDiscount,
       quantity: this.counter,
-      image: product.mainImageUrl,
-      currency: product.productVariants[0].currency.name,
+      image: this.selectedVariant.mainImageUrl || product.mainImageUrl,
+      currency: this.selectedVariant.currency.name,
     };
 
-    // Add the item to the cart using the CartService
     this.cartService.addToCart(item);
-
-    // Open the cart sidebar
     this.cartSidebarService.openCart();
   }
 }
