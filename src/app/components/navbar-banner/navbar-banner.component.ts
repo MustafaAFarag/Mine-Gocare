43e8aaf84cb54bc82fc5c7cc83fb35609db08ee6
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-06-29 13:02:37 +0300

    Navbar Country Daily Login Modal

--- src/app/components/navbar-banner/navbar-banner.component.ts
+++ src/app/components/navbar-banner/navbar-banner.component.ts
@@ -9,11 +9,13 @@ import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../services/language.service';
 import { NgIf, NgClass, isPlatformBrowser } from '@angular/common';
 import { CountryService } from '../../services/country.service';
+import { PointingSystemService } from '../../services/pointing-system.service';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-navbar-banner',
   standalone: true,
-  imports: [TranslateModule, NgIf, NgClass],
+  imports: [TranslateModule, NgIf, NgClass, DialogModule],
   templateUrl: './navbar-banner.component.html',
   styleUrl: './navbar-banner.component.css',
 })
@@ -21,11 +23,15 @@ export class NavbarBannerComponent implements OnInit {
   private languageService = inject(LanguageService);
   private platformId = inject(PLATFORM_ID);
   private countryService = inject(CountryService);
+  private pointingSystemService = inject(PointingSystemService);
   private isBrowser: boolean;
 
   isLanguageDropdownOpen = false;
   isCountryDropdownOpen = false;
   currentCountry = 'EG';
+  showPointsModal = false;
+  pointsGained = 0;
+  loadingPoints = false;
 
   constructor() {
     this.isBrowser = isPlatformBrowser(this.platformId);
@@ -67,9 +73,40 @@ export class NavbarBannerComponent implements OnInit {
   }
 
   selectCountry(country: string) {
+    // Don't add points if selecting the same country
+    if (country === this.currentCountry) {
+      this.isCountryDropdownOpen = false;
+      return;
+    }
+
     this.currentCountry = country;
     this.countryService.setCountry(country);
     this.isCountryDropdownOpen = false;
+
+    // Add points for country change
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      this.loadingPoints = true;
+      console.log('Attempting to add country change points with token:', token);
+      this.pointingSystemService.addPoints(token, 2, false).subscribe({
+        next: (pointsResponse) => {
+          this.loadingPoints = false;
+          console.log('Country change points response:', pointsResponse);
+          if (pointsResponse.success && pointsResponse.result > 0) {
+            this.pointsGained = pointsResponse.result;
+            this.showPointsModal = true;
+          }
+        },
+        error: (error) => {
+          this.loadingPoints = false;
+          console.error('Error adding country change points:', error);
+        },
+      });
+    }
+  }
+
+  handlePointsModalClose() {
+    this.showPointsModal = false;
   }
 
   getCurrentLanguage(): string {
